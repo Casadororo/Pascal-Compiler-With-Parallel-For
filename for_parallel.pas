4c2e6ed0247c1500d2857e9def2a7c139cf5ecaf
Casadororo <51424037+Casadororo@users.noreply.github.com>
2024-10-18 15:11:29 -0300

    gitignore and for parallel pas

---
+++ for_parallel.pas
@@ -0,0 +1,12 @@
+program for_parallel (input, output);
+var i, y: integer;
+begin
+  i := 0;
+  
+  for_parallel i:= 0 to 10 into 4 do
+	var i, count: integer;
+  begin
+    count := count + 1;
+    write()
+  end
+end.
