881470e57c2afb2fe9325a7c395c55de2f5143a1
Casadororo <51424037+Casadororo@users.noreply.github.com>
2024-11-26 19:22:58 -0300

    feat: old for parallel pas

---
+++ for_parallel_antigo.pas
@@ -0,0 +1,11 @@
+program alo(input, output);
+var i, y: integer;
+begin
+  for_parallel into 4 do
+	var count: integer;
+  iterate ii:= 1 to 100 do
+  begin
+    count := count + 1;
+    write(count);
+  end
+end.
